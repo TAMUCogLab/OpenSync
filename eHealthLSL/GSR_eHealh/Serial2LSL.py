18b3b397ed1fe008b619d611d92065ac39cf85c9
Moein Razavi <moeinrazavi@tamu.edu>
2021-10-15 18:34:21 -0500

    Update Serial2LSL.py

--- eHealthLSL/GSR_eHealh/Serial2LSL.py
+++ eHealthLSL/GSR_eHealh/Serial2LSL.py
@@ -1,27 +1,29 @@
 import serial
 from pylsl import StreamInfo, StreamOutlet
+import sys
 
 ser = serial.Serial(sys.argv[1], 115200)
 
-C=""
-R=""
-V=""
 
-info = StreamInfo(name='GSR', type='Voltage', channel_count=1, channel_format='string', source_id='GSR_Voltage')
+info = StreamInfo(name='eHealthGSR', type='Voltage', channel_count=3, channel_format='float32', source_id='eHealthGSR_id')
 
 outlet = StreamOutlet(info)
 
 while True:
 
+    C=-1000 # Value for the packet loss
+    R=-1000 # Value for the packet loss
+    V=-1000 # Value for the packet loss
+    
     ser_bytes = (ser.readline()).decode("utf-8")
 
     if ser_bytes[0] == "C":
-        C = ser_bytes[1:-2]
-        #print(C)
+        C = float(ser_bytes[1:-2])
+        print(C)
     elif ser_bytes[0] == "R":
-        R = ser_bytes[1:-2]
-        #print(R)
+        R = float(ser_bytes[1:-2])
+        print(R)
     elif ser_bytes[0] == "V":
-        V = ser_bytes[1:-2]
-        #print(V)
-    outlet.push_sample([V])
+        V = float(ser_bytes[1:-2])
+        print(V)
+    outlet.push_chunk([C,R,V])
