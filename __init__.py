16080b0ccddd18ab0e271a37233ac47a98e33a9c
Moein Razavi <moeinrazavi@tamu.edu>
2021-10-24 16:07:07 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,6 +10,7 @@ from .i_o import Joystick
 import subprocess
 import os
 import OpenSync
+import time
 
 def OpenSync_path():
 
@@ -20,4 +21,20 @@ def record_data(save_path):
     LabRecorder_path = OpenSync_path() + "\\LabRecorder\\LabRecorderCLI.exe"
     
     XDF_save_command = LabRecorder_path + " " + save_path + " " + "'searchstr'"
+    time.sleep(3)
     subprocess.Popen(XDF_save_command, shell=True)
+
+def stop_record():
+    try:
+        subprocess.Popen("TASKKILL /F /IM LabRecorderCLI.exe", shell=True)
+    except ValueError:
+        print("No LabRecorderCLI Task is Open!")
+    try:
+        subprocess.Popen("TASKKILL /F /IM Gazepoint.exe", shell=True)
+    except ValueError:
+        print("No Gazepoint Task is Open!")
+    try:
+        subprocess.Popen("TASKKILL /F /IM BodyBasicsLSL.exe", shell=True)
+    except ValueError:
+        print("No Kinect Task is Open!")
+        
