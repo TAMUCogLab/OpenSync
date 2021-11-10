3ccfee009cc7fd242de072b33c749b9bb2767abb
Moein Razavi <36338609+moeinrazavi@users.noreply.github.com>
2021-11-09 21:48:05 -0600

    Add files via upload

---
+++ Source Files/OpenBCI/OpenBCILSL.py
@@ -0,0 +1,25 @@
+from pyOpenBCI import OpenBCICyton
+from pylsl import StreamInfo, StreamOutlet
+import numpy as np
+import sys
+
+SCALE_FACTOR_EEG = (4500000)/24/(2**23-1) #uV/count
+SCALE_FACTOR_AUX = 0.002 / (2**4)
+
+
+info_eeg = StreamInfo('OpenBCIEEG', 'EEG', 8, 125, 'float32', 'OpenBCItestEEG')
+
+print("Creating LSL stream for AUX. \nName: OpenBCIAUX\nID: OpenBCItestEEG\n")
+
+info_aux = StreamInfo('OpenBCIAUX', 'AUX', 3, 125, 'float32', 'OpenBCItestAUX')
+
+outlet_eeg = StreamOutlet(info_eeg)
+outlet_aux = StreamOutlet(info_aux)
+
+def lsl_streamers(sample):
+    outlet_eeg.push_sample(np.array(sample.channels_data)*SCALE_FACTOR_EEG)
+    outlet_aux.push_sample(np.array(sample.aux_data)*SCALE_FACTOR_AUX)
+
+board = OpenBCICyton(port=sys.argv[1], daisy=False)
+
+board.start_stream(lsl_streamers)
